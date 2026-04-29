2c7c0f71aa3ced2be47e393d38906f3e8ade0b83
WrPages <wenderramirez00@gmail.com>
2026-04-29 17:01:05 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ MAINTENANCE_USE_ORIGINAL_IMAGE = False
 MIN_TWO_STAR_BY_GROUP = {
     "Trainer": 2,
     "Gym_Leader": 1,
-    "Elite_Four": 3,
+    "Elite_Four": 2,
 }
 CHANNEL_GROUP_MAP = {
    # 1486277594629275770: "Elite_Four",
