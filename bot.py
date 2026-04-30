df6f70fe09866a0a8232a9c661884ce6a52d4c50
WrPages <wenderramirez00@gmail.com>
2026-04-29 18:28:46 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1583,19 +1583,17 @@ async def on_message(message: discord.Message):
             online_mentions = await get_online_mentions(group)
 
             post_body = build_forum_post_text(
-                client,
-                group,
-                post_title,
-                post_image_path,
-                content="‎"
-            )            
+                result["heartbeat_meta"],
+                result["pack_label"],
+                online_mentions
+            )     
 
             post_data = await create_forum_post_with_image(
                 client,
                 group,
                 post_title,
                 post_image_path,
-                post_body
+                content="‎"
             )
 
         if post_data:
